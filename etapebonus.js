4ca2b116eb112df0581f9ee0186af52ded40abfd
Luisa <luisa.couette@coda-student.school>
2026-09-21 16:45:06 +0200

    fix: correction du bonus avec 2 fetch

--- etapebonus.js
+++ etapebonus.js
@@ -1,19 +1,33 @@
 export const etapebonus = () => {
-    fetch('https://jsonplaceholder.typicode.com/photos/418', {
-        method: 'PATCH',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "albumId": 42,
-            "title": "new photo",
-            "url": "https://live.staticflickr.com/7006/6508102407_a4de65687b_c.jpg",
-            "thumbnailUrl": "https://via.placeholder.com/150/92c952"
-        })
-    })
-    .then(response => response.json())
-    .then(json => {
-        console.log(json);
-    })
+    let url = "https://jsonplaceholder.typicode.com/photos/418";
+    let method = "PUT";
 
+    fetch(url, {
+        method: method,
+        body: JSON.stringify({
+            id: 418,
+            title: "I am a teapot",
+            url: "https://live.staticflickr.com/7006/6508102407_a4de65687b_c.jpg",
+            thumbnailUrl: "https://live.staticflickr.com/7006/6508102407_a4de65687b_c.jpg"
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then((response) => response.json())
+        .then((json) => {
+            console.log("Etape Bonus 1/2 (Photos mise à jour) :", json)
+
+            return fetch(url, {
+                method: "PATCH",
+                body: JSON.stringify({
+                    albumId: 42
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+            });
+        }).then((response) => response.json())
+        .then((json) => console.log("Etape Bonus 2/2 (Associée à l'album 42) :", json))
+        .catch((error) => console.error("Erreur :", error));
 }
